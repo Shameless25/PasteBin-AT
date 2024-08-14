b34eafeff229284b34ef25a684888cd8907ab518
Kajetan Skarubowicz <kajetanskarubowicz@gmail.com>
2024-08-14 09:16:00 +0200

    Stylistic changes

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -7,7 +7,6 @@ describe("Pastebin Test", () =>{
         await mainPage.open();
     });
     it("Creating New PasteBin with set value", async()=>{
-        const originalUrl = await browser.getUrl();
         await mainPage.mainSelectors.item('textfield').setValue('Hello from WebDriver');
         await mainPage.mainSelectors.item('dropdown').click();
         await mainPage.mainSelectors.item('options').waitForDisplayed({ timeout: 5000 });
